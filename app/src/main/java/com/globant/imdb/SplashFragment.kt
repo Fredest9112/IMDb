a2c4a3a3782bb8ac092bc38ac45a94d2e84c7a96
fredest <fredest9112@gmail.com>
2023-03-17 12:22:05 -0500

    login screen created

--- app/src/main/java/com/globant/imdb/SplashFragment.kt
+++ app/src/main/java/com/globant/imdb/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.globant.imdb
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 class SplashFragment : Fragment() {
 
@@ -14,5 +17,13 @@ class SplashFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+            val action = SplashFragmentDirections.actionSplashFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }, 2000)
     }
 }
