981b596ce94e1262b3328a419fd65cae1f43e6ba
Freddy Esteban Garzon Guayacan <freddy.garzon@globant.com>
2023-03-23 15:40:59 -0500

    error on google auth with firebase

--- app/src/main/java/com/globant/imdb/view/login/LoginFragment.kt
+++ app/src/main/java/com/globant/imdb/view/login/LoginFragment.kt
@@ -1,16 +1,33 @@
 package com.globant.imdb.view.login
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
+import com.globant.imdb.R
 import com.globant.imdb.databinding.FragmentLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginFragment : Fragment() {
 
     private var binding: FragmentLoginBinding? = null
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var launcher:ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +42,56 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding?.apply {
             loginFragment = this@LoginFragment
+            googleLogin.setOnClickListener {
+                signInGoogle()
+            }
         }
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result -> if(result.resultCode == Activity.RESULT_OK){
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            } else {
+                Log.i("result launcher","${result.resultCode}")
+                Log.i("result data", result.data.toString())
+            }
+        }
+        firebaseAuth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if(task.isSuccessful){
+            val account = task.result
+            if(account!=null){
+                updateUI(account)
+            }
+        } else {
+            Toast.makeText(context, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        Log.i("account name","${account.displayName}")
+        val credential = GoogleAuthProvider.getCredential(account.id, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if(it.isSuccessful){
+                val action = LoginFragmentDirections.actionLoginFragmentToBottomNavFragment()
+                findNavController().navigate(action)
+            } else {
+                Toast.makeText(context, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                Log.i("exception", it.exception.toString())
+            }
+        }
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
     }
 
     fun goToRegisterFragment(){
--- app/src/main/java/com/globant/imdb/view/splash/SplashFragment.kt
+++ app/src/main/java/com/globant/imdb/view/splash/SplashFragment.kt
@@ -1,15 +1,19 @@
 package com.globant.imdb.view.splash
 
 import android.os.Bundle
-import android.os.Handler
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.globant.imdb.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,9 +25,16 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Handler().postDelayed({
-            val action = SplashFragmentDirections.actionSplashFragmentToLoginFragment()
-            findNavController().navigate(action)
-        }, 2000)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val currentUser = firebaseAuth.currentUser
+        lifecycleScope.launch {
+            if(currentUser != null){
+                val action = SplashFragmentDirections.actionSplashFragmentToBottomNavFragment()
+                findNavController().navigate(action)
+            } else{
+                val action = SplashFragmentDirections.actionSplashFragmentToLoginFragment()
+                findNavController().navigate(action)
+            }
+        }
     }
 }
