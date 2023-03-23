6807668fdb3005925f73b0962df4eb345187250b
Freddy Esteban Garzon Guayacan <freddy.garzon@globant.com>
2023-03-23 11:04:01 -0500

    Adding movies to rv from searchview

--- app/src/main/java/com/globant/imdb/view/home/SearchFragment.kt
+++ app/src/main/java/com/globant/imdb/view/home/SearchFragment.kt
@@ -1,25 +1,22 @@
 package com.globant.imdb.view.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.globant.imdb.databinding.FragmentSearchBinding
 import com.globant.imdb.model.SearchMovieViewModel
 import com.globant.imdb.model.SearchMovieViewModelFactory
 import com.globant.imdb.repo.MoviesRepo
-import androidx.appcompat.widget.SearchView
-import androidx.lifecycle.lifecycleScope
 import com.globant.imdb.view.adapter.MovieAdapter
-import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
 
     private var binding: FragmentSearchBinding? = null
-    private lateinit var movieAdapter: MovieAdapter
+    private val movieAdapter = MovieAdapter()
     private val searchMovieViewModelFactory = SearchMovieViewModelFactory(MoviesRepo())
     private val searchMovieViewModel by lazy {
         ViewModelProvider(this, searchMovieViewModelFactory)[SearchMovieViewModel::class.java]
@@ -38,33 +35,31 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        movieAdapter = MovieAdapter()
-
         binding?.apply {
-            searchResultRecycler.adapter = movieAdapter
-            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String?): Boolean {
-                    if(!query.isNullOrEmpty()){
-                        lifecycleScope.launch {
-                            Log.i("query","$query")
-                            Log.i("Response","${MoviesRepo().getMoviesByQuery(query)}")
-                        }
-                        return true
+            searchMovieViewModel.apply {
+                topRatedMovies.observe(viewLifecycleOwner) {
+                    movieAdapter.submitList(it)
+                }
+                searchResultRecycler.adapter = movieAdapter
+                searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String?): Boolean {
+                        return false
                     }
-                    return false
-                }
 
-                override fun onQueryTextChange(query: String?): Boolean {
-                    return false
-                }
-            })
+                    override fun onQueryTextChange(query: String?): Boolean {
+                        if(!query.isNullOrEmpty()){
+                            getMoviesFromQuery(query)
+                            moviesFromQuery.observe(viewLifecycleOwner) {
+                                movieAdapter.submitList(it)
+                            }
+                            return true
+                        }
+                        return false
+                    }
+                })
+            }
+
         }
-
-        searchMovieViewModel.topRatedMovies.observe(viewLifecycleOwner) {
-            movieAdapter.submitList(it)
-        }
-
-
     }
 
     override fun onDestroyView() {
