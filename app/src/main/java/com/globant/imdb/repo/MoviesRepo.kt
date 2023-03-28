3745cbeb0b04b8da7a2a827078e3d36753e4d862
Freddy Esteban Garzon Guayacan <freddy.garzon@globant.com>
2023-03-28 12:57:42 -0500

    Trying to mix retrofit with coroutines.flow

--- app/src/main/java/com/globant/imdb/repo/MoviesRepo.kt
+++ app/src/main/java/com/globant/imdb/repo/MoviesRepo.kt
@@ -1,12 +1,12 @@
 package com.globant.imdb.repo
 
 import android.util.Log
-import com.globant.imdb.data.Constants.API_KEY
 import com.globant.imdb.api.IMDbNetworking
+import com.globant.imdb.data.Constants.API_KEY
 import com.globant.imdb.data.Movie
+import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import javax.inject.Inject
 
 class MoviesRepo @Inject constructor() {
     suspend fun getTopRatedMovies(): List<Movie> {
@@ -15,8 +15,8 @@ class MoviesRepo @Inject constructor() {
             try {
                 val response = IMDbNetworking.movieData.getTopRatedMoviesAsync(
                     API_KEY
-                ).await()
-                topRatedMovies = response.results
+                )
+                topRatedMovies = response.await().results
             } catch (e: Exception) {
                 Log.e("Error receiving top rated movies", "${e.message}")
             }
@@ -24,14 +24,22 @@ class MoviesRepo @Inject constructor() {
         return topRatedMovies ?: emptyList()
     }
 
+//    fun getTopRatedMovies(): LiveData<List<Movie>> {
+//        return liveData {
+//            IMDbNetworking.movieData.getTopRatedMoviesAsync(API_KEY).collect{
+//                emit(it.await().results)
+//            }
+//        }
+//    }
+
     suspend fun getMoviesByQuery(query: String): List<Movie> {
         var movieListByQuery: List<Movie>? = null
         withContext(Dispatchers.IO) {
             try {
                 val response = IMDbNetworking.movieData.getMoviesFromQueryAsync(
                     apiKey = API_KEY, query = query
-                ).await()
-                movieListByQuery = response.results
+                )
+                movieListByQuery = response.await().results
             } catch (e: Exception) {
                 Log.e("Error receiving movies by search", "${e.message}")
             }
